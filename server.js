73539f078a0cbd1d1e9cb60bd4002be8dc468e9a
Phillip Valdez <Phillip0150@gmail.com>
2019-07-11 22:39:56 -0500

    Updated styles

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var PORT = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static("public"));
+app.use("/public", express.static("public"));
 
 app.use(express.urlencoded({extended: true }));
 app.use(express.json());
